18e0a19da66ce103959b428313bce74db4e5b77b
limq <limq@cgnfs.com.cn>
2019-01-24 15:08:17 +0800

    再来个js

--- entry.js
+++ entry.js
@@ -1,1 +1,2 @@
+require("./second.js");
 document.getElementById("app").innerHTML = "这是entry.js里的一段话";
---
+++ second.js
@@ -0,0 +1,3 @@
+var h2 = document.createElement("h2");
+h2.innerHTML = "来自second。js的一段话";
+document.body.append(h2);
